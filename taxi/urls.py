4a9aecb58771b9f9de252fde9abb14abbb1165a4
Oleksandr Taranenko <105813721+GGsosna@users.noreply.github.com>
2024-08-18 17:28:48 +0300

    Update urls.py

--- taxi/urls.py
+++ taxi/urls.py
@@ -48,7 +48,7 @@ urlpatterns = [
     path("cars/<int:pk>/update/", CarUpdateView.as_view(), name="car-update"),
     path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
-    path("drivers/create", DriverCreateView.as_view(), name="driver-create"),
+    path("drivers/create/", DriverCreateView.as_view(), name="driver-create"),
     path(
         "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
     ),
